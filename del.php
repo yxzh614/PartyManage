abb5b4b65e24e4272d08eb9332b7393b434e2bea
yxzh614 <526686022@qq.com>
2017-05-21 17:08:36 +0800

    test git bash

--- del.php
+++ del.php
@@ -1,7 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php
+    session_start();//huihua
+    include("footer/footer_head.php");
+    require_once("config.php");
+    if(isset($_COOKIE["PHPSESSID"])){
+        session_id($_COOKIE["PHPSESSID"]);
+        if(isset($_SESSION["right"]) &&$_SESSION["right"]==0){
+            if(isset($_POST["submit"])&&$_POST["submit"]) {
+                $checkbox = $_POST['onetodel'];
+                for ($i = 0; $i < count($checkbox); $i++) {
+                    $sqlToDel = "DELETE FROM `personnelinformation` WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
+                    echo $sqlToDel;
+                }
+            }else{
+                echo 33;
+            }
+        }else{
+            ?>
+            <script>
+                alert("未登录或权限不足！");
+                window.location = "../sign-in.php";
+            </script>
+            <?php
+        }
+    }
+    else{
+    ?>
+    <script>
+        window.location = "../sign-in.php";
+    </script>
 <?php
-/**
- * Created by PhpStorm.
- * User: ak-hyeon-chal
- * Date: 17/4/26
- * Time: 19:11
- */
+}
