fa61f5be50bc5034d401e5635d734f3dafe6718a
yxzh614 <526686022@qq.com>
2017-06-07 21:32:35 +0800

    学生表的修改

--- del.php
+++ del.php
@@ -8,54 +8,61 @@
     require_once("config.php");
     if(isset($_COOKIE["PHPSESSID"])){
         session_id($_COOKIE["PHPSESSID"]);
-        if(isset($_SESSION["right"]) &&$_SESSION["right"]==0){
-            if(isset($_POST["submit"])&&$_POST["submit"]) {
+        if (isset($_SESSION["right"]) && $_SESSION["right"] == 0){
+            if (isset($_POST["submit"]) && $_POST["submit"]) {
                 switch ($_POST["type"]) {
-                    case 'del':{
-                        $checkbox = $_POST['onetodel'];
-                        for ($i = 0; $i < count($checkbox); $i++) {
-                            $sqlToDel = "DELETE FROM `personnelinformation` WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
-                            echo $sqlToDel;
-                        }
-                    }break;
-                    case 'save_LJJ_time':{
-                        $checkbox = $_POST['onetodel'];
-                        for ($i = 0; $i < count($checkbox); $i++) {
-                            $sqlToDel = "UPDATE `personnelinformation` SET `LJJ_time`='" . $_POST["LJJ_time"] . "' WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
-                            echo $sqlToDel;
-                        }
-                    }break;
-                    case 'out':{
-                        $checkbox = $_POST['onetodel'];
-                        for ($i = 0; $i < count($checkbox); $i++) {
-                            $sqlToDel = "UPDATE `personnelinformation` SET `out_time`='" . $_POST["out_time"] . "',`gowhere`='" . $_POST["gowhere"] . "',`state`='" . $_POST["state"] . "' WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
-                        if (mysqli_query($db, $sqlToDel)) {
-                            // echo "==插入成功==";
-                            echo "<script>alert('调出成功！');window.location = \"Right_1/1_pmm_outside.php\";</script>";
-                        }
-                        ?>
-                        <script>
-                            //alert("数据不能为空！");
-                            //window.location = "1_DRM_stu_list.php";
-                        </script>
-                    <?php
-                        }
-                    }break;
+                    case 'del': {
+                    $checkbox = $_POST['onetodel'];
+                    for ($i = 0; $i < count($checkbox); $i++) {
+                        $sqlToDel = "DELETE FROM `personnelinformation` WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
+                        echo $sqlToDel;
+                    }
+                }break;
+                    case 'save_LJJ_time': {
+                    $checkbox = $_POST['onetodel'];
+                    for ($i = 0; $i < count($checkbox); $i++) {
+                        $sqlSaveLJJTime = "UPDATE `personnelinformation` SET `LJJ_time`='" . $_POST["LJJ_time"] . "' WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
+                        echo $sqlSaveLJJTime;
+                    }
+                }break;
+                    case 'out': {
+                $checkbox = $_POST['onetodel'];
+            for ($i = 0;
+                 $i < count($checkbox);
+                 $i++) {
+                $sqlToDel = "UPDATE `personnelinformation` SET `out_time`='" . $_POST["out_time"] . "',`gowhere`='" . $_POST["gowhere"] . "',`state`='" . $_POST["state"] . "' WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
+                if (mysqli_query($db, $sqlToDel)) {
+                    // echo "==插入成功==";
+                    echo "<script>alert('调出成功！');window.location = \"Right_1/1_pmm_outside.php\";</script>";
                 }
-
+                ?>
+                <script>
+                    //alert("数据不能为空！");
+                    //window.location = "1_DRM_stu_list.php";
+                </script>
+            <?php
+            }
+            }break;
+                    case 'save_JJPX_time': {
+                $checkbox = $_POST['onetodel'];
+                for ($i = 0; $i < count($checkbox); $i++) {
+                    $sqlToDel = "UPDATE `personnelinformation` SET `LJJ_time`='" . $_POST["JJPX_time"] . "',`LJJ_time`='" . $_POST["JJPX_time"] . "' WHERE `personnelinformation`.`ID_number` = '" . $checkbox[$i] . "'";
+                    echo $sqlToDel;
+                }
+            }break;
+                }
             }else{
                 echo 33;
             }
         }else{
-            ?>
+        ?>
             <script>
                 alert("未登录或权限不足！");
                 window.location = "../sign-in.php";
             </script>
             <?php
         }
-    }
-    else{
+    }else{
     ?>
     <script>
         window.location = "../sign-in.php";
--- footer/footer_pmd_activist.php
+++ footer/footer_pmd_activist.php
@@ -1,30 +1,87 @@
+<?php
+require_once ("../config.php");
+if(!isset($_SESSION)){ session_start(); }
+if(isset($_COOKIE["PHPSESSID"])){
+    session_id($_COOKIE["PHPSESSID"]);
+    if(isset($_SESSION["right"])&&$_SESSION["right"]==0){
+
+        if(isset($_POST["submit"])&&$_POST["submit"]) {
+            $sqlUpdateStu = "";
+            echo $sqlUpdateStu;
+            if (mysqli_query($db, $sqlUpdateStu)) {
+                ?>
+                <script>
+                    alert('添加成功！');
+                    window.location="<?php echo $_POST['url']?>";
+                </script>
+                <?php
+            }
+            ?>
+            <script>
+                //alert("数据不能为空！");
+                //window.location = "1_DRM_stu_list.php";
+            </script>
+            <?php
+        }
+        ?>
+        <?php
+    }else{
+        ?>
+        <script>
+            alert("未登录或权限不足！");
+            window.location = "../sign-in.php";
+        </script>
+        <?php
+    }
+}
+else{
+    ?>
+    <script>
+        window.location = "../sign-in.php";
+    </script>
+    <?php
+}
+
+?>
+
 <h2>入党积极分子阶段信息</h2>
-<div class="btn-toolbar">
-    <button class="btn btn-primary" ><font color="#F7F8F7">保存</font></button>
-</div>
+<form action="../footer/footer_pmd_activist.php?ID=<?php echo $_GET["ID"];?>" method="post" id="edit">
+    <input type="hidden" name="url" value="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']; ?>">
+    <div class="btn-toolbar">
+        <input class="btn btn-primary" type="submit" name="submit" value="保存" onclick="">
+    </div>
 <div class="well">
     <div align="center">
     
       <table width="694" >
         <tbody>
-        
-          <tr align="right">
-            <td width="231">支部确定入党积极分子会议：</td>
-            <td width="203" align="left"><select name="ZQ_positivemeet" class="input-medium">
-        		<option value="会议ID">会议主题1</option>
-            	<option value="会议ID">会议主题2</option>
-       	 	</select></td>
-            <td width="92">团推优时间：</td>
-            <td width="103" align="left"><input type="date" name="Tmember_meet_time" class="input-medium"></td>
-          </tr>
-          <tr align="right">
-            <td>入党积极分子分子培训时间：</td>
-            <td align="left"><input type="date" name="JJPX_time" class="input-medium"></td>
-            <td>培训成绩：</td>
-            <td align="left"><input type="text" name="JJPX_mark" class="input-medium"></td>
-          </tr>
+        <?php
+        $sqlGetStuR1="SELECT Tmember_meet_time,JJPX_time,JJPX_mark FROM personnelinformation WHERE personnelinformation.ID_number='".$_GET["ID"]."'";
+        if($resGSR1=mysqli_query($db,$sqlGetStuR1)){
+            while ($rowsGSR1=mysqli_fetch_assoc($resGSR1)){
+                ?>
+                <tr align="right">
+                    <td width="231">支部确定入党积极分子会议：</td>
+                    <td width="203" align="left"><select name="ZQ_positivemeet" class="input-medium">
+                            <option value="会议ID">会议主题1</option>
+                            <option value="会议ID">会议主题2</option>
+                        </select></td>
+                    <td width="92">团推优时间：</td>
+                    <td width="103" align="left"><input type="date" name="Tmember_meet_time" class="input-medium" value="<?php echo $rowsGSR1['Tmember_meet_time'];?>"></td>
+                </tr>
+                <tr align="right">
+                    <td>入党积极分子分子培训时间：</td>
+                    <td align="left"><input type="date" name="JJPX_time" class="input-medium" value="<?php echo $rowsGSR1['JJPX_time'];?>"></td>
+                    <td>培训成绩：</td>
+                    <td align="left"><input type="text" name="JJPX_mark" class="input-medium" value="<?php echo $rowsGSR1['JJPX_mark'];?>"></td>
+                </tr>
+                <?php
+            }
+        }
+        ?>
+
         </tbody>
       </table>
     </div>
 </div>
-  
+</form>
